99345f319d1bf16e37b4817fc0993c478caf424a
Pulzze92 <pulzze92@yahoo.com>
2024-01-27 18:10:58 -0500

    feat: added bcrypt and corrected registration

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ import mongoose from 'mongoose';
 import { registerValidator } from './validations/auth.js';
 import { validationResult } from 'express-validator';
 
+import UserModel from './models/User.js';
+
+import bcrypt from 'bcrypt';
+
 mongoose
-    .connect('mongodb+srv://admin:6iSVmziwD7NMXAg@cluster0.wxgj0mi.mongodb.net/?retryWrites=true&w=majority')
+    .connect('mongodb+srv://admin:6iSVmziwD7NMXAg@cluster0.wxgj0mi.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('DB is OK'))
     .catch((err) => console.log('DB ERROR', err));
 
@@ -14,15 +18,34 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidator, (req, res) => {
-    const errors = validationResult(req);
+app.post('/auth/register', registerValidator, async (req, res) => {
+    try {
+        const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array());
     }
 
-    res.json({
-        success: true,
-    })
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash,
+    });
+
+    const user = await doc.save();
+
+    res.json(user)
+    
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: 'Unsuccessful registration'
+        });
+    }
 });
 
 app.listen(4444, (err) => {
--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,8 @@
 import { body } from 'express-validator';
 
 export const registerValidator = [
-    body('email').isEmail(),
-    body('password').isLength({ min: 5 }),
-    body('fullName').isLength({ min: 3}),
-    body('avatarUrl').optional().isURL(),
+    body('email', 'incorrect email format').isEmail(),
+    body('password', 'password cannot be shorter than 5 symbols').isLength({ min: 5 }),
+    body('fullName', 'full name must be 3 letters minimum').isLength({ min: 3}),
+    body('avatarUrl', 'the link to avatar is incorrect').optional().isURL(),
 ];
